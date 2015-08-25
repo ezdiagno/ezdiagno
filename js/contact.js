3b01f3838474b5adbdb3e2d76993bc36625bb360
ashish-thinkpad <thinkpad@vyavsaay.com>
2015-08-25 15:17:39 +0530

    updated mailing

--- js/contact.js
+++ js/contact.js
@@ -1,60 +1,65 @@
 // JavaScript Document
 
-
 <!-- 
 //Browser Support Code
-function contact_form(){
- var ajaxRequest;  // The variable that makes Ajax possible!
-	
- try{
-   // Opera 8.0+, Firefox, Safari
-   ajaxRequest = new XMLHttpRequest();
- }catch (e){
-   // Internet Explorer Browsers
-   try{
-      ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
-   }catch (e) {
-      try{
-         ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
-      }catch (e){
-         // Something went wrong
-         alert("Your browser broke!");
-         return false;
-      }
-   }
- }
- // Create a function that will receive data 
- // sent from the server and will update
- // div section in the same page.
- ajaxRequest.onreadystatechange = function(){
-   if(ajaxRequest.readyState == 4){
-      var ajaxDisplay = document.getElementById('Msg'); /// Msg is a div userd for message display
-      ajaxDisplay.innerHTML = ajaxRequest.responseText;
-   }
- }
- // Now get the value from user and pass it to
- // server script.
- 
- //var name = document.getElementById('name').value;
- //var poc = document.getElementById('poc').value;
- //var phone = document.getElementById('phone').value;
- var email = document.getElementById('email').value; 
- //var address = document.getElementById('address').value;
- //var user_message = document.getElementById('user_message').value;
-  
- 
- var queryString = "?email=" + email;
- ajaxRequest.open("GET", "contact.php" + queryString, true);
- ajaxRequest.send(null); 
- 
- 	//document.getElementById('name').value = "";
-	//document.getElementById('poc').value = "";
- 	//document.getElementById('phone').value ="";
- 	document.getElementById('email').value = "";
- 	//document.getElementById('address').value ="";
-	//document.getElementById('user_message').value = "";
- 	
+function contact_form()
+{
+	var email = document.getElementById('email').value;   
 
- 
+	$.ajax(
+	{
+		type: “POST”,
+		url: “https://mandrillapp.com/api/1.0/messages/send.json”,
+		data: 
+		{
+			‘key’: ‘ouQ9d2s4hE-5QqzBwZHUMw’,
+    		‘message’: {
+      	‘from_email’: 'info@ezdiagno.com',
+      	‘to’: 
+      	[
+          {
+            ‘email’: email
+            ‘type’: ‘to’
+          }
+			],
+		‘autotext’: ‘true’,
+      ‘subject’: ‘EZdiagno Received Your Enquiry’,
+      ‘html’: ‘<b>Dear Sir/ Mam</b><br/><br/><br/>Thank you for getting in touch with us. We appreciate your time.<br/>Our team member will get back to you shortly on your query/comment.<br/><br/><br/>Thanks and Regards<br/>Team Ezdiagno’
+		}
+	}
+}).done(function(response) 
+{
+	var ajaxDisplay = document.getElementById('Msg'); /// Msg is a div userd for message display
+	ajaxDisplay.innerHTML = 'Thank you for registering with EZdiagno. We will be looking forward to your further engagement';
+});
+	
+	$.ajax(
+	{
+		type: “POST”,
+		url: “https://mandrillapp.com/api/1.0/messages/send.json”,
+		data: 
+		{
+			‘key’: ‘ouQ9d2s4hE-5QqzBwZHUMw’,
+    		‘message’: {
+      	‘from_email’: email,
+      	‘to’: 
+      	[
+          {
+            ‘email’: 'ezdiagno@gmail.com'
+            ‘type’: ‘to’
+          },
+          {
+            ‘email’: 'info@ezdiagno.com'
+            ‘type’: ‘to’
+          }
+			],
+		‘autotext’: ‘true’,
+      ‘subject’: ‘Enquiry from website’,
+      ‘html’: ‘Enquiry from email-id: ’+email
+		}
+	}
+});
+
+	document.getElementById('email').value = "";
 }
 //-->
